028584b1eef36bd896c47d43ecc16a593bb88812
eidam6969 <adamlips26@gmail.com>
2026-02-26 09:36:05 +0100

    flask

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,28 @@
-import flask
+from flask import Flask, render_template, url_for
 
-app = flask.Flask(__name__)
-@app.route("/")
+app = Flask(__name__)
+
+@app.route("/1")
 def hello():
     return "Hello World!"
+
+@app.route("/2")
+def ahoj():
+    return render_template("index2.html")
+
+@app.route("/3")
+def text():
+    return render_template("index3.html")
+
+@app.route("/4")
+def python():
+    text = "Ahoj, světe z proměnné!"
+    return render_template("index4.html", message=text)
+
+@app.route("/5")
+def obrazek():
+    image_url = url_for('static', filename='images/img1.jpg')
+    return render_template("index5.html", image_url=image_url)
 
 if __name__ == "__main__":
     app.run()
